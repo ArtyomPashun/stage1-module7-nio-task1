457145f0bd1a04926e80c2888b1280d42b30df13
Artsiom Pashun <a.pashun@syberry.com>
2023-04-19 11:35:57 +0200

    Finished module 7, task 1

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,37 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder strBuilder = new StringBuilder();
+
+        try (InputStream fileInputStream = Files.newInputStream(file.toPath())) {
+            int ch;
+            while ((ch = fileInputStream.read()) != -1) {
+                strBuilder.append((char) ch);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String resultStr = strBuilder.toString();
+        String[] strArray = resultStr.split("\r\n");
+        return fillProfile(strArray);
+    }
+
+    public Profile fillProfile(String[] arr) {
+        Profile profile = new Profile();
+
+        profile.setName(arr[0].replace("Name: ", ""));
+        profile.setAge(Integer.valueOf(arr[1].replace("Age: ", "")));
+        profile.setEmail(arr[2].replace("Email: ", ""));
+        profile.setPhone(Long.valueOf(arr[3].replace("Phone: ", "")));
+
+        return profile;
     }
 }
